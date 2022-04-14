7e95e4eab3938ccea070e2853f6703541827b92b
“kiranrathor30697” <kiranrathor30697@gmail.com>
2022-04-14 15:16:45 +0530

    header responsive try

--- src/Layouts/Header.js
+++ src/Layouts/Header.js
@@ -5,13 +5,13 @@ import '../css/common.css'
 export default function Header() {
   return (
     <>
-        <div className="p-3" style={{backgroundColor:"#17202A"}}>
-            <div className="container">
+        <div className="p-3" style={{backgroundColor:"#17202A"}}>{/* 
+            <div className="container"> */}
                 <Row>
-                    <Col className="text-white" md={6}>
+                    <Col className="text-white" md={6} sm={4} xs={4}>
                         <Image className="text-center mt-3" width="120"src={require('../img/header.png')} alt={"Image Not Found"}/>
                     </Col>
-                    <Col className="text-white d-flex justify-content-end k_fontfamily" md={6}>
+                    <Col className="text-white d-flex justify-content-end k_fontfamily" md={6} sm={8} xs={8}>
                         <div>
                             <h5 className="fw-bolder lh-1 k_fontfamily k_color text-end">KKR SOLUTION MALHARGARH</h5>
                             <h6 className="ms-5 lh-1 k_fontfamily k_color text-end">Ward No 13 Char Bhuja Marg Malhargarh </h6>
@@ -21,8 +21,8 @@ export default function Header() {
                             <h6 className="ms-5 lh-1 k_fontfamily k_color text-end">State: 23-Madhya Pradesh</h6>
                         </div>
                     </Col>
-                </Row>
-            </div>
+                </Row>{/* 
+            </div> */}
         </div>
     </>
   )
