01ebcf6c44cc5e7a57cf6753e875a52a0a9bbb70
“kiranrathor30697” <kiranrathor30697@gmail.com>
2022-04-14 16:34:02 +0530

    create form layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import {BrowserRouter as Router , Routes, Route} from 'react-router-dom';
 import Header from './Layouts/Header';
+import CreateBill from './pages/CreateBill';
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Header />} />
+        <Route path="/" element={<CreateBill />} />
+        <Route path="/header" element={<Header />} />
+        <Route path="/createbill" element={<CreateBill />} />
         {/* <Route path="/login" element={<Login />} /> */}
       </Routes>
     </Router>
